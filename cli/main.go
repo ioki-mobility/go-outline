3b0f2ee52a78ea2a697a523675d1e60419452dc9
Stefan M <StefMa@users.noreply.github.com>
2023-07-26 10:07:32 +0200

    Intial CLI app with ability to fetch collections (#16)
    The cli is written with the `cobra` package
    and can be invoked via the `outline` command.
    
    For now, we support a `collection` subcommand
    to operate on anything related outline collections.
    
    The `fetch` subcommand on the `collection` command can be
    triggered to print a specific collection in json format to stdout.

--- cli/main.go
+++ cli/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ioki-mobility/go-outline/internal/cli"
+)
+
+func main() {
+	if err := cli.Run(); err != nil {
+		fmt.Println("fatal error: ", err)
+		os.Exit(1)
+	}
+}
