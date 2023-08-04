5bee9b9b80daa7171409971997452b3fa67c18ef
StefMa <StefMaDev@outlook.com>
2023-08-04 13:03:03 +0200

    Add support for creating a collection [cli]

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -45,13 +45,16 @@ func collectionCmd(rootCmd *cobra.Command) *cobra.Command {
 	fetchCommand := collectionCmdFetch(rootCmd)
 	collectionCmd.AddCommand(fetchCommand)
 
+	createCommand := collectionCmdCrate(rootCmd)
+	collectionCmd.AddCommand(createCommand)
+
 	return collectionCmd
 }
 
 func collectionCmdFetch(rootCmd *cobra.Command) *cobra.Command {
 	return &cobra.Command{
 		Use:   "fetch",
-		Short: "Fetch collection details for given ID",
+		Short: "Fetch collection details",
 		Long:  "Fetch collection details for given ID and prints as json to stdout",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -79,6 +82,43 @@ func collectionCmdFetch(rootCmd *cobra.Command) *cobra.Command {
 				}
 				fmt.Println(string(b))
 			}
+			return nil
+		},
+	}
+}
+
+func collectionCmdCrate(rootCmd *cobra.Command) *cobra.Command {
+	return &cobra.Command{
+		Use:   "create",
+		Short: "Creates a collection",
+		Long:  "Creates a collection with the given name and prints the result as json to stdout",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			baseUrl, err := rootCmd.Flags().GetString(flagNameBaseUrl)
+			if err != nil {
+				return fmt.Errorf("required flag '%s' not set: %w", flagNameBaseUrl, err)
+			}
+			apiKey, err := rootCmd.Flags().GetString(flagNameApiKey)
+			if err != nil {
+				return fmt.Errorf("required flag '%s' not set: %w", flagNameApiKey, err)
+			}
+			client := outline.New(baseUrl, &http.Client{}, apiKey)
+
+			collectionName := args[0]
+			collection, err := client.
+				Collections().
+				Create(collectionName).
+				Do(context.Background())
+			if err != nil {
+				return fmt.Errorf("can't create collection with name '%s': %w", collectionName, err)
+			}
+
+			b, err := json.Marshal(&collection)
+			if err != nil {
+				return fmt.Errorf("failed marshalling collection with name '%s: %w", collectionName, err)
+			}
+			fmt.Println(string(b))
+
 			return nil
 		},
 	}
