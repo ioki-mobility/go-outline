1787fd4f16557f267896478ba81651598f5b0c32
Stefan M <StefMa@users.noreply.github.com>
2023-08-17 15:24:49 +0200

    Reorganize CLI parsing (#50)

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -16,25 +16,21 @@ const (
 )
 
 func Run() error {
-	rootCmd := rootCmd()
-	collectionCmd := collectionCmd(rootCmd)
-	rootCmd.AddCommand(collectionCmd)
-
-	return rootCmd.Execute()
+	return parseCmd().Execute()
 }
 
-func rootCmd() *cobra.Command {
+type config struct {
+	serverUrl string
+	apiKey    string
+}
+
+func parseCmd() *cobra.Command {
 	rootCmd := &cobra.Command{Use: "outline"}
 
-	var apiKey string
-	var serverURL string
-	rootCmd.PersistentFlags().StringVar(&serverURL, flagServerURL, "", "The outline API server url")
-	rootCmd.PersistentFlags().StringVar(&apiKey, flagApiKey, "", "The outline api key")
+	var cfg config
+	rootCmd.PersistentFlags().StringVar(&cfg.serverUrl, flagServerURL, "", "The outline API server url")
+	rootCmd.PersistentFlags().StringVar(&cfg.apiKey, flagApiKey, "", "The outline api key")
 
-	return rootCmd
-}
-
-func collectionCmd(rootCmd *cobra.Command) *cobra.Command {
 	collectionCmd := &cobra.Command{
 		Use:   "collection",
 		Short: "Work with collections",
@@ -42,117 +38,90 @@ func collectionCmd(rootCmd *cobra.Command) *cobra.Command {
 		Args:  cobra.MinimumNArgs(1),
 	}
 
-	docsSubCmd := collectionCmdDocs(rootCmd)
-	collectionCmd.AddCommand(docsSubCmd)
-
-	infoSubCmd := collectionCmdInfo(rootCmd)
-	collectionCmd.AddCommand(infoSubCmd)
-
-	createSubCmd := collectionCmdCreate(rootCmd)
-	collectionCmd.AddCommand(createSubCmd)
-
-	return collectionCmd
-}
-
-func collectionCmdDocs(rootCmd *cobra.Command) *cobra.Command {
-	return &cobra.Command{
-		Use:   "docs",
-		Short: "Get document structure",
-		Long:  "Get a summary of associated documents (and children)",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			baseUrl, err := rootCmd.Flags().GetString(flagServerURL)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagServerURL, err)
-			}
-			apiKey, err := rootCmd.Flags().GetString(flagApiKey)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagApiKey, err)
-			}
-
-			client := outline.New(baseUrl, &http.Client{}, apiKey)
-			for _, colId := range args {
-				st, err := client.Collections().DocumentStructure(outline.CollectionID(colId)).Do(context.Background())
-				if err != nil {
-					return fmt.Errorf("can't get collection with id '%s': %w", colId, err)
-				}
-
-				b, err := json.MarshalIndent(st, "", "  ")
-				if err != nil {
-					return fmt.Errorf("failed marshalling collection with id '%s: %w", colId, err)
-				}
-				fmt.Println(string(b))
-			}
-			return nil
-		},
-	}
-}
-
-func collectionCmdInfo(rootCmd *cobra.Command) *cobra.Command {
-	return &cobra.Command{
+	collectionInfoCmd := &cobra.Command{
 		Use:   "info",
 		Short: "Get collection info",
 		Long:  "Get information about the collection",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			baseUrl, err := rootCmd.Flags().GetString(flagServerURL)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagServerURL, err)
-			}
-			apiKey, err := rootCmd.Flags().GetString(flagApiKey)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagApiKey, err)
-			}
-
-			client := outline.New(baseUrl, &http.Client{}, apiKey)
-			for _, colId := range args {
-				col, err := client.Collections().Get(outline.CollectionID(colId)).Do(context.Background())
-				if err != nil {
-					return fmt.Errorf("can't get collection with id '%s': %w", colId, err)
-				}
-
-				b, err := json.MarshalIndent(col, "", "  ")
-				if err != nil {
-					return fmt.Errorf("failed marshalling collection with id '%s: %w", colId, err)
-				}
-				fmt.Println(string(b))
-			}
-			return nil
+			return collectionInfo(cfg.serverUrl, cfg.apiKey, args)
 		},
 	}
-}
 
-func collectionCmdCreate(rootCmd *cobra.Command) *cobra.Command {
-	return &cobra.Command{
+	collectionCreateCmd := &cobra.Command{
 		Use:   "create",
 		Short: "Creates a collection",
 		Long:  "Creates a collection with the given name and prints the result as json to stdout",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			baseUrl, err := rootCmd.Flags().GetString(flagServerURL)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagServerURL, err)
-			}
-			apiKey, err := rootCmd.Flags().GetString(flagApiKey)
-			if err != nil {
-				return fmt.Errorf("required flag '%s' not set: %w", flagApiKey, err)
-			}
-
-			client := outline.New(baseUrl, &http.Client{}, apiKey)
-
-			name := args[0]
-			col, err := client.Collections().Create(name).Do(context.Background())
-			if err != nil {
-				return fmt.Errorf("can't create collection with name '%s': %w", name, err)
-			}
-
-			b, err := json.MarshalIndent(col, "", "  ")
-			if err != nil {
-				return fmt.Errorf("failed marshalling collection with name '%s: %w", name, err)
-			}
-			fmt.Println(string(b))
-
-			return nil
+			return collectionCreate(cfg.serverUrl, cfg.apiKey, args[0])
 		},
 	}
+
+	collectionDocumentsCmd := &cobra.Command{
+		Use:   "docs",
+		Short: "Get document structure",
+		Long:  "Get a summary of associated documents (and children)",
+		Args:  cobra.MinimumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return collectionDocuments(cfg.serverUrl, cfg.apiKey, args)
+		},
+	}
+
+	rootCmd.AddCommand(collectionCmd)
+	collectionCmd.AddCommand(collectionInfoCmd)
+	collectionCmd.AddCommand(collectionCreateCmd)
+	collectionCmd.AddCommand(collectionDocumentsCmd)
+
+	return rootCmd
+}
+
+func collectionInfo(serverUrl string, apiKey string, ids []string) error {
+	oc := outline.New(serverUrl, &http.Client{}, apiKey)
+	for _, id := range ids {
+		col, err := oc.Collections().Get(outline.CollectionID(id)).Do(context.Background())
+		if err != nil {
+			return fmt.Errorf("can't get collection with id '%s': %w", id, err)
+		}
+
+		b, err := json.MarshalIndent(col, "", "  ")
+		if err != nil {
+			return fmt.Errorf("failed marshalling collection with id '%s: %w", id, err)
+		}
+		fmt.Println(string(b))
+	}
+	return nil
+}
+
+func collectionCreate(serverUrl string, apiKey string, name string) error {
+	oc := outline.New(serverUrl, &http.Client{}, apiKey)
+	col, err := oc.Collections().Create(name).Do(context.Background())
+	if err != nil {
+		return fmt.Errorf("can't create collection with name '%s': %w", name, err)
+	}
+
+	b, err := json.MarshalIndent(col, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed marshalling collection with name '%s: %w", name, err)
+	}
+	fmt.Println(string(b))
+
+	return nil
+}
+
+func collectionDocuments(serverUrl string, apiKey string, ids []string) error {
+	oc := outline.New(serverUrl, &http.Client{}, apiKey)
+	for _, id := range ids {
+		st, err := oc.Collections().DocumentStructure(outline.CollectionID(id)).Do(context.Background())
+		if err != nil {
+			return fmt.Errorf("can't get collection with id '%s': %w", id, err)
+		}
+
+		b, err := json.MarshalIndent(st, "", "  ")
+		if err != nil {
+			return fmt.Errorf("failed marshalling collection with id '%s: %w", id, err)
+		}
+		fmt.Println(string(b))
+	}
+	return nil
 }
