79c28ccd21d215ed75648e22ac0ef7ffbdc125e8
Stefan M <StefMa@users.noreply.github.com>
2023-08-30 09:08:56 +0200

    CLI support for list collections (#60)
    Fetch one page of collection at a time, print it one by one and
    then fetch next page until no more pages are left.  Any error at
    the start or in the middle would stop the process and the error
    would be printed in the end.

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -77,10 +77,20 @@ func parseCmd() *cobra.Command {
 		},
 	}
 
+	collectionListCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List all collections",
+		Long:  "Get a list of all collections.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return collectionList(cfg.serverUrl, cfg.apiKey)
+		},
+	}
+
 	rootCmd.AddCommand(collectionCmd)
 	collectionCmd.AddCommand(collectionInfoCmd)
 	collectionCmd.AddCommand(collectionCreateCmd)
 	collectionCmd.AddCommand(collectionDocumentsCmd)
+	collectionCmd.AddCommand(collectionListCmd)
 
 	return rootCmd
 }
@@ -132,5 +142,27 @@ func collectionDocuments(serverUrl string, apiKey string, ids []string) error {
 		}
 		fmt.Println(string(b))
 	}
+	return nil
+}
+
+func collectionList(serverUrl string, apiKey string) error {
+	oc := outline.New(serverUrl, &http.Client{}, apiKey)
+	err := oc.Collections().List().Do(context.Background(), func(col *outline.Collection, err error) (bool, error) {
+		if err != nil {
+			return false, err
+		}
+
+		b, err := json.MarshalIndent(col, "", "  ")
+		if err != nil {
+			return false, fmt.Errorf("failed marshalling collection: %w", err)
+		}
+		fmt.Println(string(b))
+
+		return true, nil
+	})
+	if err != nil {
+		return fmt.Errorf("can't get list of collections: %w", err)
+	}
+
 	return nil
 }
