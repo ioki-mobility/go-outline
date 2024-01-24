4affe695220a24e6c12e4d5d5c938b6fdaa0059e
StefMa <StefMaDev@outlook.com>
2024-01-24 08:15:35 +0100

    [HTTP] Support for update collections

--- package_test.go
+++ package_test.go
@@ -203,6 +203,99 @@ func TestClientCollectionsGet(t *testing.T) {
 	assert.Equal(t, &expected.Data, got)
 }
 
+func TestClientCollectionsUpdate_failed(t *testing.T) {
+	tests := map[string]struct {
+		isTemporary bool
+		rt          http.RoundTripper
+	}{
+		"HTTP request failed": {
+			isTemporary: false,
+			rt: &testutils.MockRoundTripper{
+				RoundTripFn: func(r *http.Request) (*http.Response, error) {
+					return nil, &net.DNSError{}
+				},
+			},
+		},
+		"server side error": {
+			isTemporary: true,
+			rt: &testutils.MockRoundTripper{
+				RoundTripFn: func(r *http.Request) (*http.Response, error) {
+					return &http.Response{
+						Request:       r,
+						StatusCode:    http.StatusServiceUnavailable,
+						ContentLength: -1,
+						Body:          io.NopCloser(strings.NewReader("service unavailable")),
+					}, nil
+				},
+			},
+		},
+		"client side error": {
+			isTemporary: false,
+			rt: &testutils.MockRoundTripper{
+				RoundTripFn: func(r *http.Request) (*http.Response, error) {
+					return &http.Response{
+						Request:       r,
+						ContentLength: -1,
+						StatusCode:    http.StatusUnauthorized,
+						Body:          io.NopCloser(strings.NewReader("unauthorized key")),
+					}, nil
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			hc := &http.Client{}
+			hc.Transport = test.rt
+			cl := outline.New(testServerURL, hc, testApiKey)
+			col, err := cl.Collections().Update("collection id").Do(context.Background())
+			assert.Nil(t, col)
+			require.NotNil(t, err)
+			assert.Equal(t, test.isTemporary, outline.IsTemporary(err))
+		})
+	}
+}
+
+func TestClientCollectionsUpdate(t *testing.T) {
+	testResponse := exampleCollectionsUpdateResponse
+
+	// Prepare HTTP client with mocked transport.
+	hc := &http.Client{}
+	hc.Transport = &testutils.MockRoundTripper{RoundTripFn: func(r *http.Request) (*http.Response, error) {
+		// Assert request method and URL.
+		assert.Equal(t, http.MethodPost, r.Method)
+		u, err := url.JoinPath(common.BaseURL(testServerURL), common.CollectionsUpdateEndpoint())
+		require.NoError(t, err)
+		assert.Equal(t, u, r.URL.String())
+
+		testAssertHeaders(t, r.Header)
+		testAssertBody(t, r, fmt.Sprintf(`{"id":"%s", "name":"%s", "description":"%s"}`, "collection id", "NewName", "New Desc"))
+
+		return &http.Response{
+			Request:       r,
+			ContentLength: -1,
+			StatusCode:    http.StatusOK,
+			Body:          io.NopCloser(strings.NewReader(testResponse)),
+		}, nil
+	}}
+
+	cl := outline.New(testServerURL, hc, testApiKey)
+	got, err := cl.Collections().
+		Update("collection id").
+		Name("NewName").
+		Description("New Desc").
+		Do(context.Background())
+	require.NoError(t, err)
+
+	// Manually unmarshal test response and see if we get same object via the API.
+	expected := &struct {
+		Data outline.Collection `json:"data"`
+	}{}
+	require.NoError(t, json.Unmarshal([]byte(testResponse), expected))
+	assert.Equal(t, &expected.Data, got)
+}
+
 func TestClientCollectionsList(t *testing.T) {
 	requestCount := atomic.Uint32{}
 	hc := &http.Client{}
@@ -708,6 +801,8 @@ const exampleCollectionsGetResponse string = `{
     "deletedAt": "2019-08-24T14:15:22Z"
   }
 }`
+
+const exampleCollectionsUpdateResponse string = exampleCollectionsGetResponse
 
 const exampleCollectionsListResponse_2collections string = `
 	{
