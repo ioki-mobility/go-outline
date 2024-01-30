276804931826b35cbe72d5762368ecb95fff14d3
Stefan M <StefMa@users.noreply.github.com>
2024-01-30 16:32:31 +0100

    Introduce User model (#73)

--- models.go
+++ models.go
@@ -20,26 +20,38 @@ type DocumentSummary struct {
 
 // Document represents an outline document.
 type Document struct {
-	ID               DocumentID    `json:"id"`
-	CollectionID     CollectionID  `json:"collectionId"`
-	ParentDocumentID DocumentID    `json:"parentDocumentId"`
-	Title            string        `json:"title"`
-	FullWidth        bool          `json:"fullWidth"`
-	Emoji            string        `json:"emoji"`
-	Text             string        `json:"text"`
-	URLID            string        `json:"urlId"`
-	Collaborators    []interface{} `json:"collaborators"`
-	Pinned           bool          `json:"pinned"`
-	Template         bool          `json:"template"`
-	TemplateID       TemplateID    `json:"templateId"`
-	Revision         int           `json:"revision"`
-	CreatedAt        time.Time     `json:"createdAt"`
-	CreatedBy        interface{}   `json:"createdBy"`
-	UpdatedAt        time.Time     `json:"updatedAt"`
-	UpdatedBy        interface{}   `json:"updatedBy"`
-	PublishedAt      time.Time     `json:"publishedAt"`
-	ArchivedAt       time.Time     `json:"archivedAt"`
-	DeletedAt        time.Time     `json:"deletedAt"`
+	ID               DocumentID   `json:"id"`
+	CollectionID     CollectionID `json:"collectionId"`
+	ParentDocumentID DocumentID   `json:"parentDocumentId"`
+	Title            string       `json:"title"`
+	FullWidth        bool         `json:"fullWidth"`
+	Emoji            string       `json:"emoji"`
+	Text             string       `json:"text"`
+	URLID            string       `json:"urlId"`
+	Collaborators    []User       `json:"collaborators"`
+	Pinned           bool         `json:"pinned"`
+	Template         bool         `json:"template"`
+	TemplateID       TemplateID   `json:"templateId"`
+	Revision         int          `json:"revision"`
+	CreatedAt        time.Time    `json:"createdAt"`
+	CreatedBy        User         `json:"createdBy"`
+	UpdatedAt        time.Time    `json:"updatedAt"`
+	UpdatedBy        User         `json:"updatedBy"`
+	PublishedAt      time.Time    `json:"publishedAt"`
+	ArchivedAt       time.Time    `json:"archivedAt"`
+	DeletedAt        time.Time    `json:"deletedAt"`
+}
+
+// User represents an outline user.
+type User struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	AvatarURL    string    `json:"avatarUrl"`
+	Email        string    `json:"email"`
+	IsAdmin      bool      `json:"isAdmin"`
+	IsSuspended  bool      `json:"isSuspended"`
+	LastActiveAt time.Time `json:"lastActiveAt"`
+	CreatedAt    time.Time `json:"createdAt"`
 }
 
 // Collection represents an outline collection.
